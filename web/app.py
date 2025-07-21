ad1cc94c6176ead4154e7a0fe665bd5cf4569aac
Shnezzin <sebastian.kraehe@googlemail.com>
2025-07-21 23:47:56 +0200

    CSP fix

--- web/app.py
+++ web/app.py
@@ -76,7 +76,7 @@ csp = {
         'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
         'https://cdn.jsdelivr.net/npm/chart.js',
         'https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js',
-        "'unsafe-inline'"  # Required for inline scripts
+        "'strict-dynamic'"
     ],
     'style-src': [
         "'self'",
